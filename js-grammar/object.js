345088d4ee66177610400599fcfd59bb6724f659
bigdra50 <ryu1998tea@gmail.com>
2019-12-28 21:28:45 +0900

    wip

--- js-grammar/object.js
+++ js-grammar/object.js
@@ -1,21 +1,20 @@
-var startTime = null;
-var displayArea = document.getElementById("display-area");
-
-function start(){
-    startTime = Date.now();
-    document.body.onkeypress = stop;
-}
-
-function stop() {
-    var currentTime = Date.now();
-    var sec = (currentTime - startTime) / 1000;
-    if(9.5 <= sec && sec <= 10.5){
-        displayArea.innerText = sec+"!";
-    }else{
-        displayArea.innerText = sec+"...";
+var game = {
+    startTime : null,
+    displayArea:  document.getElementById("display-area"),
+    start: function () {
+            game.startTime = Date.now();
+            document.body.onkeypress = game.stop;
+    },
+    stop: function () {
+        var currentTime = Date.now();
+        var sec = (currentTime - game.startTime) / 1000;
+        if (9.5 <= sec && sec <= 10.5) {
+            game.displayArea.innerText = sec + "!";
+        } else {
+            game.displayArea.innerText = sec + "...";
+        }
     }
-}
-
-if(confirm("OKを押して10秒だと思ったら何かキーを押してください")){
-    start();
+};
+if (confirm("OKを押して10秒だと思ったら何かキーを押してください")) {
+    game.start();
 }
