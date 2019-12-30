bcba67b252d704bd5f9ff3d4fb17837b4e5a763f
bigdra50 <ryu1998tea@gmail.com>
2019-12-30 17:00:16 +0900

    unko

--- assessment/assessment.js
+++ assessment/assessment.js
@@ -38,7 +38,7 @@ assessmentButton.onclick = () => {
     anchor.setAttribute('href', hrefValue);
     anchor.className = 'twitter-hashtag-button';
     anchor.setAttribute('data-text', result);
-    anchor.innerText = 'Tweet #hogehoge';
+    anchor.innerText = 'Tweet #unko';
     resultDivided.appendChild(anchor);
 
     // widget.jsの設定
@@ -48,22 +48,20 @@ assessmentButton.onclick = () => {
 };
 
 const answers = [
-    '{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。',
-    '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
-    '{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます。',
-    '{userName}のいいところは厳しさです。{userName}の厳しさがものごとをいつも成功に導きます。',
-    '{userName}のいいところは知識です。博識な{userName}を多くの人が頼りにしています。',
-    '{userName}のいいところはユニークさです。{userName}だけのその特徴が皆を楽しくさせます。',
-    '{userName}のいいところは用心深さです。{userName}の洞察に、多くの人が助けられます。',
-    '{userName}のいいところは見た目です。内側から溢れ出る{userName}の良さに皆が気を惹かれます。',
-    '{userName}のいいところは決断力です。{userName}がする決断にいつも助けられる人がいます。',
-    '{userName}のいいところは思いやりです。{userName}に気をかけてもらった多くの人が感謝しています。',
-    '{userName}のいいところは感受性です。{userName}が感じたことに皆が共感し、わかりあうことができます。',
-    '{userName}のいいところは節度です。強引すぎない{userName}の考えに皆が感謝しています。',
-    '{userName}のいいところは好奇心です。新しいことに向かっていく{userName}の心構えが多くの人に魅力的に映ります。',
-    '{userName}のいいところは気配りです。{userName}の配慮が多くの人を救っています。',
-    '{userName}のいいところはその全てです。ありのままの{userName}自身がいいところなのです。',
-    '{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。',
+    '{userName}の特徴的なうんこは皆を惹きつけ、心に残ります。',
+    '{userName}のうんこに見つめられた人は、気になって仕方がないでしょう。',
+    '{userName}のうんこは情熱的です。{userName}のうんこの情熱に周りの人は感化されます。',
+    '{userName}のうんこの厳しさがものごとをいつも成功に導きます。',
+    '{userName}の博識なうんこを多くの人が頼りにしています。',
+    '{userName}のうんこはユニークです。{userName}だけのその特徴が皆を楽しくさせます。',
+    '{userName}のうんこの洞察に、多くの人が助けられます。',
+    '{userName}のうんこのいいところは見た目です。内側から溢れ出る{userName}うんこの良さに皆が気を惹かれます。',
+    '{userName}のうんこに気をかけてもらった多くの人が感謝しています。',
+    '{userName}のうんこが感じたことに皆が共感し、わかりあうことができます。',
+    '強引すぎない{userName}のうんこの考えに皆が感謝しています。',
+    '新しいことに向かっていく{userName}のうんこの心構えが多くの人に魅力的に映ります。',
+    'ありのままの{userName}うんこ自身がいいところなのです。',
+    'やばいと思ったときにしっかりと衝動を抑えられる{userName}うんこが皆から評価されています。',
 ];
 
 /**
